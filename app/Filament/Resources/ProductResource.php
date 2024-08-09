c1bd7d32495f186dd4f9111a223ba23761c50c2a
Rpebs pramesta <rpebspramesta@gmail.com>
2024-08-09 08:18:22 +0700

    grouping nav menu

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -29,7 +29,9 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-circle-stack';
+
+    protected static ?string $navigationGroup = 'Master Data';
 
     public static function form(Form $form): Form
     {
